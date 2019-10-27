1422c7853a8cb30e14477ba1e72460df3dec0936
utnso <utnso@ubuntu-server>
2019-10-27 10:01:55 -0300

    pilas de ready y blocked creadas

--- SUSE/src/suse.c
+++ SUSE/src/suse.c
@@ -4,8 +4,28 @@
 #include<commons/string.h>
 #include<commons/bitarray.h>
 #include<commons/config.h>
+struct nodoPila{
+	int dato;
+	struct nodoPila *ptrsiguiente;
+};
+typedef struct nodoPila NodoPila;
+typedef struct NodoPila *ptrNodoPila
+void empujar(ptrNodoPila *ptrcima, int valor){
+	ptrNodoPila ptrNuevo;
+	ptrNuevo = malloc(sizeof(NodoPila));
+	if(ptrcima != NULL){
+		ptrNuevo->dato = valor;
+			ptrNuevo->ptrsiguiente = *ptrcima;
+			ptrcima = ptrNuevo;
+	}else{
+		printf("memoria insuficiente");
+	}
+}
+
 int main(){
 	t_log* logger;
+	ptrNodoPila ready = NULL;
+	ptrNodoPila blocked = NULL;
 	logger = log_create("suse.log","suse.c",1,LOG_LEVEL_INFO);
 	return 0;
 }
