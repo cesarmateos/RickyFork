619e36ebe0973ae9f468773aef39485a234a8d5b
César <41212957+cesarmateos@users.noreply.github.com>
2019-10-31 21:32:33 -0300

    Update SAC-servidor.c

--- FUSE/src/SAC-servidor.c
+++ FUSE/src/SAC-servidor.c
@@ -5,9 +5,13 @@
 #include<commons/bitarray.h>
 #include<commons/config.h>
 
-typedef int BlockSize[4096];
+#define BLOCKSIZE 4096
+
 typedef int ptrGBloque;
 t_log* logger;
+char discoActual[71];
+const int cantidadNodos = 1024;
+
 
 typedef struct {
 	char identificador[3];
@@ -27,57 +31,70 @@ typedef struct {
 	ptrGBloque arrayPunteros[1000];
 }TablaNodos;
 
+/*
 typedef struct{
 	Header encabezado;
 	t_bitarray bitmap; //ver como cuerno funciona (es de la commons)
 	TablaNodos tablaNodos;
-	BlockSize bloqueDatos;
+	int bloquesDatos[];
 }HD;
+*/
 
-char discoActual[];
 
-void crearHD(char nombreHD[],int tamanioHD){
-	int tamanioMaximoDeHD = 16384; //en bytes
-	if(tamanioHD > tamanioMaximoDeHD){
+void crearHD(char nombreHD[],int tamanioHDenMB){
+	int tamanioMaximoDeHD = 1677216; //en Megabytes
+
+	if(tamanioHDenMB > tamanioMaximoDeHD){
 		printf("El tamaño del disco supera el máximo soportado\n");
-		log_error(logger, "No se pudo crear disco de almacenamiento debido a que superaba el tamaño máximo soportado\n");
+		//log_error(logger, "No se pudo crear disco de almacenamiento debido a que superaba el tamaño máximo soportado\n");
 	}else{
+		int tamanioHDenBytes = tamanioHDenMB*1024*1024;
+		int cantidadDeBloques = ( sizeof(tamanioHDenBytes)/BLOCKSIZE ) - 1 ;
+		void* bits = malloc(1);
+		bitarray_create(bits, (cantidadDeBloques/8) );
+		TablaNodos tablas[cantidadNodos];
+		Header encabezado = {
+				"SAC",
+				1,
+				1,
+				"n",
+		};
+
+
 		FILE  *disco;
 		disco = fopen(("%s",nombreHD),"wb");
 		if(!disco){
 			printf("Error al crear el disco\n");
 		}else{
 
-		Header encabezado;
-		strcopy(encabezado.identificador,"SAC");
-		encabezado.identificador = 1;
-		encabezado.bloqueInicioBitmap = 1;
-		strcopy(encabezado.tamanio, "n");
-
-		char str[tamanioHD*1024*1024*1024];
-		fwrite(str,sizeof(char),sizeof(str),disco);
+		fwrite(&encabezado,sizeof(formato),1,disco);
 		fflush(disco);
 		fclose(disco);
-		printf("Disco  %s de %d Gigabytes creado exitosamente.\n",nombreHD,tamanioHD)
-		log_info(logger,"Disco %s de %d Gigabytes creado exitosamente.\n",nombreHD,tamanioHD);
+		printf("Disco  %s de %d Megabytes creado y exitosamente.\n",nombreHD,tamanioHDenMB);
+		//log_info(logger,("Disco %s de %d Gigabytes creado exitosamente.\n",nombreHD,tamanioHD));
 		}
 	}
 }
 
 void formatearHD(char nombreHD[]){
 	FILE *disco;
+	int cantidadDeBloques;
 	disco = fopen(("%s",nombreHD),"r+b");
 	if(!disco){
-				printf("No se pudo abrir o no existe el disco %s.\n",nombreHD);
-			}else{
-
-			}
+		printf("No se pudo abrir o no existe el disco %s.\n",nombreHD);
+	}else{
+		cantidadDeBloques = sizeof(disco)/4096;
+	}
 
 }
 
-void seleccionarHD(char path[]){
-	discoActual = path;
-	log_info(logger, "Se seleccionó el disco %s.\n",path);
+void seleccionarHD(char disco[]){
+	//discoActual = disco;
+	//log_info(logger, "Se seleccionó el disco %s.\n",disco);
+}
+
+void mostrarHDActual(){
+	//printf("El disco actual es el siguiente: %s.\n",discoActual);
 }
 
 void escribir(){
@@ -90,9 +107,9 @@ void borrar(){
 }
 
 int main(){
-
-	logger = log_create("SAC-Servicor.log","SAC-servidor.c",1,LOG_LEVEL_INFO);
-
-
+	//logger = log_create("SAC-Servicor.log","SAC-servidor.c",1,LOG_LEVEL_INFO);
+	crearHD("Disco",1);
+	//mostrarHDActual;
+	printf("Compilo");
 	return 0;
 }
