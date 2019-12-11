c177b54304d2b958cc3a8a752b6f3f0a56748b53
utnso <utnso@ubuntu-server>
2019-12-11 20:03:04 -0300

    hecho el ordenamiento por sjf

--- SUSE/src/suse.c
+++ SUSE/src/suse.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include<commons/log.h>
 #include<commons/string.h>
 #include<commons/bitarray.h>
@@ -14,6 +15,8 @@ static int gradoDeMutiprogramacion;
 struct t_hilo{
 	int identificador;
 	char* tiempoDeEjecucion;
+	int tiempoReal;
+	int tiempoEstimado;
 };
 typedef struct t_hilo T_hilo;
 
@@ -23,6 +26,12 @@ struct t_programa{
 	T_hilo* exec;
 };
 typedef struct t_programa T_programa;
+
+struct t_semaforo{
+	char identificador;
+	int valor;
+};
+typedef struct t_semaforo T_semaforo;
 
 void iniciarServidor(){
 	SocketServer_Start("SUSE",3801);
@@ -44,15 +53,23 @@ void ejecutarHilo(T_programa* programa,T_hilo* hiloAejecutar){
 	programa->exec = hiloAejecutar;
 }
 
-T_hilo crearHilo(int identificador){
+T_hilo crearHilo(int identificador,int tiempoEstimado){
 	T_hilo hilo;
 	hilo.identificador = identificador;
 	hilo.tiempoDeEjecucion = temporal_get_string_time();
+	hilo.tiempoEstimado = tiempoEstimado;
+	hilo.tiempoReal = 0;
 	return hilo;
 }
 
 T_hilo* proximoHiloAejecutar(T_programa programa){
 	return list_get(programa.ready,0);
+}
+
+bool sjf(T_hilo* hiloA,T_hilo* hiloB){
+	int a = hiloA->tiempoEstimado * 0.5 + hiloA->tiempoReal *0.5;
+	int b = hiloB->tiempoEstimado * 0.5 + hiloB->tiempoReal *0.5;
+	return a > b;
 }
 
 void CargarPrograma(T_programa* programa,t_list* lista){
@@ -66,8 +83,14 @@ void bloquearHilo(T_programa* programa,t_list* blockeado){
 }
 int main() {
 	t_list* listaPrograma = list_create();
-	T_programa programa1 = crearPrograma(1);
-	list_add(listaPrograma,&programa1);
+	T_hilo hiloA = crearHilo(1,3);
+	T_hilo hiloB = crearHilo(2,2);
+	T_hilo* ptr;
+	list_add(listaPrograma,&hiloA);
+	list_add(listaPrograma,&hiloB);
+	list_sort(listaPrograma,sjf);
+	ptr = list_get(listaPrograma,0);
+	printf("el hilo mas chico es %d\n",ptr->identificador);
 	t_list* blockeados = list_create();
 	return EXIT_SUCCESS;
 }
